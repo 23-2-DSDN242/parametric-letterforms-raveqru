8dcf2d3717051b667cee35b5eb89e3190d8c6594
Anna Giniiatullina <131623439+raveqru@users.noreply.github.com>
2024-04-24 16:28:26 +1200

    Update letters.js

--- letters.js
+++ letters.js
@@ -5,64 +5,337 @@ const alphabet = {
     "offsety": 0
   },
   "A": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
+    //"stroke1_posX": 1,
+    "stroke1_posY": 5,
+    "stroke1_width": 20,
+    "stroke1_height": 190,
+    //"stroke1_corner": 20,
+    "angle_set": 0,
+
+
+    "right_posX": 50,
+    "right_posY": 1,
+    "right_width": 30,
+    "right_height": 199,
+    "right_corner": 20,
+    "angle_set2": -10,
+
+
+    // "topRectX": 0,
+    "topRectY": 100,
+    "topWidth": 70,
+    "topHeight": 30,
+
+
+    "stroke2_posX": 60,
+    "stroke2_posY": 110,
+    "stroke2_width": 60,
+    "stroke2_height": 20,
+
+
+
   },
   "B": {
-    "size": 75,
-    "offsetx": 0,
-    "offsety": -70
+    //"stroke1_posX": 1,
+    "stroke1_posY": 110,
+    "stroke1_width": 70,
+    "stroke1_height": 80,
+    //"stroke1_corner": 20,
+    "angle_set": 180,
+
+    "right_posX": 60,
+    "right_posY": 1,
+    "right_width": 30,
+    "right_height": 194,
+    "right_corner": 20,
+    "angle_set2": -180,
+
+
+    // "topRectX": 0,
+    "topRectY": 148,
+    "topWidth": 40,
+    "topHeight": 60,
+
+
+    "stroke2_posX": 55,
+    "stroke2_posY": 145,
+    "stroke2_width": 70,
+    "stroke2_height": 94,
+
+
   },
   "C": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    //"stroke1_posX": 1,
+    "stroke1_posY": 110,
+    "stroke1_width": 95,
+    "stroke1_height": 30,
+    //"stroke1_corner": 20,
+    "angle_set": 90,
+
+    "right_posX": 65,
+    "right_posY": 1,
+    "right_width": 30,
+    "right_height": 186,
+    //"right_corner": 20,
+    "angle_set2": -180,
+
+
+    // "topRectX": 0,
+    "topRectY": 178,
+    "topWidth": 80,
+    "topHeight": 40,
+
+
+    "stroke2_posX": 50,
+    "stroke2_posY": 33,
+    "stroke2_width": 80,
+    "stroke2_height": 30,
+
   },
   "D": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    //"stroke1_posX": 1,
+    "stroke1_posY": 6,
+    "stroke1_width": 79,
+    "stroke1_height": 23,
+    //"stroke1_corner": 20,
+    "angle_set": 180,
+
+    "right_posX": 0,
+    "right_posY": 32,
+    "right_width": 30,
+    "right_height": 120,
+    //"right_corner": 20,
+    "angle_set2": -180,
+
+
+    // "topRectX": 0,
+    "topRectY": 25,
+    "topWidth": 85,
+    "topHeight": 40,
+
+
+    "stroke2_posX": 23,
+    "stroke2_posY": 107,
+    "stroke2_width": 15,
+    "stroke2_height": 110,
+
   },
   "E": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    //"stroke1_posX": 1,
+    "stroke1_posY": 12,
+    "stroke1_width": 81,
+    "stroke1_height": 38,
+    //"stroke1_corner": 20,
+    "angle_set": 360,
+
+    "right_posX": 5,
+    "right_posY": 5,
+    "right_width": 31,
+    "right_height": 190,
+    //"right_corner": 20,
+    "angle_set2": -360,
+
+
+    // "topRectX": 0,
+    "topRectY": 100,
+    "topWidth": 86,
+    "topHeight": 41,
+
+
+    "stroke2_posX": 50,
+    "stroke2_posY": 166,
+    "stroke2_width": 77,
+    "stroke2_height": 34,
+
   },
   "F": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    //"stroke1_posX": 1,
+    "stroke1_posY": 10,
+    "stroke1_width": 28,
+    "stroke1_height": 77,
+    //"stroke1_corner": 20,
+    "angle_set": 0,
+
+    "right_posX": 61,
+    "right_posY": 1,
+    "right_width": 31,
+    "right_height": 102,
+    //"right_corner": 20,
+    "angle_set2": -180,
+
+
+    // "topRectX": 0,
+    "topRectY": 24,
+    "topWidth": 92,
+    "topHeight": 39,
+
+
+    "stroke2_posX": 49,
+    "stroke2_posY": 74,
+    "stroke2_width": 80,
+    "stroke2_height": 28,
   },
   "G": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    //"stroke1_posX": 1,
+    "stroke1_posY": 10,
+    "stroke1_width": 14,
+    "stroke1_height": 179,
+    //"stroke1_corner": 20,
+    "angle_set": 0,
+
+    "right_posX": 12,
+    "right_posY": 162,
+    "right_width": 55,
+    "right_height": 28,
+    //"right_corner": 20,
+    "angle_set2": -180,
+
+
+    // "topRectX": 0,
+    "topRectY": 130,
+    "topWidth": 38,
+    "topHeight": 37,
+
+
+    "stroke2_posX": 51,
+    "stroke2_posY": 160,
+    "stroke2_width": 71,
+    "stroke2_height": 60,
   },
   "H": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+  //"stroke1_posX": 1,
+  "stroke1_posY": 5,
+  "stroke1_width": 25,
+  "stroke1_height": 79,
+  //"stroke1_corner": 20,
+  "angle_set": 0,
+
+  "right_posX": 4,
+  "right_posY": 4,
+  "right_width": 31,
+  "right_height": 192,
+  //"right_corner": 20,
+  "angle_set2": -360,
+
+
+  // "topRectX": 0,
+  "topRectY": 119,
+  "topWidth": 91,
+  "topHeight": 45,
+
+
+  "stroke2_posX": 79,
+  "stroke2_posY": 100,
+  "stroke2_width": 23,
+  "stroke2_height": 182,
   },
   "I": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+  //"stroke1_posX": 1,
+  "stroke1_posY": 168,
+  "stroke1_width": 82,
+  "stroke1_height": 23,
+  //"stroke1_corner": 20,
+  "angle_set": 180,
+
+  "right_posX": 31,
+  "right_posY": 2,
+  "right_width": 35,
+  "right_height": 192,
+  //"right_corner": 20,
+  "angle_set2": -360,
+
+
+  // "topRectX": 0,
+  "topRectY": 98,
+  "topWidth": 37,
+  "topHeight": 120,
+
+
+  "stroke2_posX": 48,
+  "stroke2_posY": 176,
+  "stroke2_width": 83,
+  "stroke2_height": 26,
   },
   "J": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+ //"stroke1_posX": 1,
+ "stroke1_posY": 160,
+ "stroke1_width": 72,
+ "stroke1_height": 30,
+ //"stroke1_corner": 20,
+ "angle_set": 180,
+
+ "right_posX": 58,
+ "right_posY": 30,
+ "right_width": 33,
+ "right_height": 164,
+ //"right_corner": 20,
+ "angle_set2": -360,
+
+
+ // "topRectX": 0,
+ "topRectY": 180,
+ "topWidth": 78,
+ "topHeight": 32,
+
+
+ "stroke2_posX": 26,
+ "stroke2_posY": 140,
+ "stroke2_width": 20,
+ "stroke2_height": 26,
   },
   "K": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+  //"stroke1_posX": 1,
+  "stroke1_posY": 80,
+  "stroke1_width": 54,
+  "stroke1_height": 28,
+  //"stroke1_corner": 20,
+  "angle_set": 126,
+
+  "right_posX": 50,
+  "right_posY": 0,
+  "right_width": 32,
+  "right_height": 76,
+  //"right_corner": 20,
+  "angle_set2": -209,
+
+
+  // "topRectX": 0,
+  "topRectY": 124,
+  "topWidth": 32,
+  "topHeight": 30,
+
+
+  "stroke2_posX": 22,
+  "stroke2_posY": 100,
+  "stroke2_width": 28,
+  "stroke2_height": 184,
   },
   "L": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+//"stroke1_posX": 1,
+"stroke1_posY": 12,
+"stroke1_width": 78,
+"stroke1_height": 32,
+//"stroke1_corner": 20,
+"angle_set": 180,
+
+"right_posX": 58,
+"right_posY": 54,
+"right_width": 36,
+"right_height": 138,
+//"right_corner": 20,
+"angle_set2": -180,
+
+
+// "topRectX": 0,
+"topRectY": 174,
+"topWidth": 88,
+"topHeight": 38,
+
+
+"stroke2_posX": 30,
+"stroke2_posY": 76,
+"stroke2_width": 33,
+"stroke2_height": 136,
   },
   "M": {
     "size": 50,
@@ -70,14 +343,58 @@ const alphabet = {
     "offsety": 0
   },
   "N": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+ //"stroke1_posX": 1,
+"stroke1_posY": 8,
+"stroke1_width": 18,
+"stroke1_height": 182,
+//"stroke1_corner": 20,
+"angle_set": 0,
+
+"right_posX": 30,
+"right_posY": 0,
+"right_width": 29,
+"right_height": 186,
+//"right_corner": 20,
+"angle_set2": -14,
+
+
+// "topRectX": 0,
+"topRectY": 100,
+"topWidth": 14,
+"topHeight": 14,
+
+
+"stroke2_posX": 80,
+"stroke2_posY": 98,
+"stroke2_width": 16,
+"stroke2_height": 180,
   },
   "O": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+ //"stroke1_posX": 1,
+"stroke1_posY": 40,
+"stroke1_width": 19,
+"stroke1_height": 113,
+//"stroke1_corner": 20,
+"angle_set": 180,
+
+"right_posX": 3,
+"right_posY": 38,
+"right_width": 28,
+"right_height": 126,
+//"right_corner": 20,
+"angle_set2": -360,
+
+
+// "topRectX": 0,
+"topRectY": 20,
+"topWidth": 75,
+"topHeight": 28,
+
+
+"stroke2_posX": 49,
+"stroke2_posY": 180,
+"stroke2_width": 56,
+"stroke2_height": 18,
   },
   "P": {
     "size": 50,
@@ -85,44 +402,220 @@ const alphabet = {
     "offsety": 0
   },
   "Q": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+  //"stroke1_posX": 1,
+"stroke1_posY": 10,
+"stroke1_width": 79,
+"stroke1_height": 158,
+//"stroke1_corner": 20,
+"angle_set": 360,
+
+"right_posX": 59,
+"right_posY": 2,
+"right_width": 31,
+"right_height": 62,
+//"right_corner": 20,
+"angle_set2": -220,
+
+
+// "topRectX": 0,
+"topRectY": 90,
+"topWidth": 41,
+"topHeight": 58,
+
+
+"stroke2_posX": 48,
+"stroke2_posY": 90,
+"stroke2_width": 33,
+"stroke2_height": 60,
   },
   "R": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+  //"stroke1_posX": 1,
+"stroke1_posY": 10,
+"stroke1_width": 78,
+"stroke1_height": 77,
+//"stroke1_corner": 20,
+"angle_set": 0,
+
+"right_posX": 25,
+"right_posY": 0,
+"right_width": 34,
+"right_height": 92,
+//"right_corner": 20,
+"angle_set2": -194,
+
+
+// "topRectX": 0,
+"topRectY": 48,
+"topWidth": 48,
+"topHeight": 48,
+
+
+"stroke2_posX": 23,
+"stroke2_posY": 102,
+"stroke2_width": 27,
+"stroke2_height": 176,
   },
   "S": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+ //"stroke1_posX": 1,
+"stroke1_posY": 8,
+"stroke1_width": 54,
+"stroke1_height": 68,
+//"stroke1_corner": 20,
+"angle_set": 0,
+
+"right_posX": 21,
+"right_posY": 106,
+"right_width": 35,
+"right_height": 16,
+//"right_corner": 20,
+"angle_set2": -126,
+
+
+// "topRectX": 0,
+"topRectY": 160,
+"topWidth": 36,
+"topHeight": 36,
+
+
+"stroke2_posX": 64,
+"stroke2_posY": 156,
+"stroke2_width": 52,
+"stroke2_height": 70,
   },
   "T": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+//"stroke1_posX": 1,
+"stroke1_posY": 10,
+"stroke1_width": 80,
+"stroke1_height": 29,
+//"stroke1_corner": 20,
+"angle_set": 0,
+
+"right_posX": 9,
+"right_posY": 12,
+"right_width": 78,
+"right_height": 26,
+//"right_corner": 20,
+"angle_set2": -360,
+
+
+// "topRectX": 0,
+"topRectY": 118,
+"topWidth": 31,
+"topHeight": 142,
+
+
+"stroke2_posX": 59,
+"stroke2_posY": 122,
+"stroke2_width": 22,
+"stroke2_height": 134,
   },
   "U": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+//"stroke1_posX": 1,
+"stroke1_posY": 8,
+"stroke1_width": 21,
+"stroke1_height": 152,
+//"stroke1_corner": 20,
+"angle_set": 0,
+
+"right_posX": 10,
+"right_posY": 168,
+"right_width": 72,
+"right_height": 22,
+//"right_corner": 20,
+"angle_set2": 0,
+
+
+// "topRectX": 0,
+"topRectY": 184,
+"topWidth": 70,
+"topHeight": 20,
+
+
+"stroke2_posX": 78,
+"stroke2_posY": 84,
+"stroke2_width": 22,
+"stroke2_height": 150,
   },
   "V": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+//"stroke1_posX": 1,
+"stroke1_posY": 8,
+"stroke1_width": 26,
+"stroke1_height": 182,
+//"stroke1_corner": 20,
+"angle_set": 0,
+
+"right_posX": 28,
+"right_posY": 0,
+"right_width": 33,
+"right_height": 196,
+//"right_corner": 20,
+"angle_set2": -162,
+
+
+// "topRectX": 0,
+"topRectY": 32,
+"topWidth": 0,
+"topHeight": 0,
+
+
+"stroke2_posX": 23,
+"stroke2_posY": 178,
+"stroke2_width": 0,
+"stroke2_height": 0,
   },
   "W": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+  //"stroke1_posX": 1,
+"stroke1_posY": 8,
+"stroke1_width": 25,
+"stroke1_height": 182,
+//"stroke1_corner": 20,
+"angle_set": 0,
+
+"right_posX": 4,
+"right_posY": 2,
+"right_width": 34,
+"right_height": 192,
+//"right_corner": 20,
+"angle_set2": -180,
+
+
+// "topRectX": 0,
+"topRectY": 186,
+"topWidth": 25,
+"topHeight": 12,
+
+
+"stroke2_posX": 49,
+"stroke2_posY": 154,
+"stroke2_width": 25,
+"stroke2_height": 36,
   },
   "X": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+//"stroke1_posX": 1,
+"stroke1_posY": 82,
+"stroke1_width": 24,
+"stroke1_height": 107,
+//"stroke1_corner": 20,
+"angle_set": 320,
+
+"right_posX": 30,
+"right_posY": 0,
+"right_width": 38,
+"right_height": 198,
+//"right_corner": 20,
+"angle_set2": -161,
+
+
+// "topRectX": 0,
+"topRectY": 124,
+"topWidth": 0,
+"topHeight": 0,
+
+
+"stroke2_posX": 35,
+"stroke2_posY": 136,
+"stroke2_width": 2,
+"stroke2_height": 2,
   },
   "Y": {
     "size": 50,
